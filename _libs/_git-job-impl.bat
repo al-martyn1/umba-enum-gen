5d198f9003eb4fdc6c35c8ab9f6d82a447bf1c15
al-martyn1 <amart@mail.ru>
2025-05-07 22:52:56 +0300

    Присунул либ по YAML'у

--- _libs/_git-job-impl.bat
+++ _libs/_git-job-impl.bat
@@ -2,9 +2,11 @@
 @call :DO_GIT_JOB %1 %~dp0\marty_cpp
 @call :DO_GIT_JOB %1 %~dp0\marty_tr
 @call :DO_GIT_JOB %1 %~dp0\marty_utf
+@call :DO_GIT_JOB %1 %~dp0\marty_yaml_toml_json
 @call :DO_GIT_JOB %1 %~dp0\nlohmann
 @call :DO_GIT_JOB %1 %~dp0\sfmt
 @call :DO_GIT_JOB %1 %~dp0\umba
+@call :DO_GIT_JOB %1 %~dp0\yaml-cpp
 
 
 @exit /B
