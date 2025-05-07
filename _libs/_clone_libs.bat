f9b885a70778f0122268667c55bab613b059ba04
al-martyn1 <amart@mail.ru>
2025-05-07 17:24:59 +0300

    Добавил либы marty_tr, marty_utf

--- _libs/_clone_libs.bat
+++ _libs/_clone_libs.bat
@@ -27,7 +27,10 @@ set PREFIX=git@%BASE%:%ACC%
 
 git clone %PREFIX%/umba_mm_mod_encodings.git   %~dp0\encoding
 git clone %PREFIX%/marty_cpp.git               %~dp0\marty_cpp
+git clone %PREFIX%/marty_tr.git                %~dp0\marty_tr
+git clone %PREFIX%/marty_utf.git               %~dp0\marty_utf
 git clone %PREFIX%/umba_mm_mod_sfmt.git        %~dp0\sfmt
 git clone %PREFIX%/umba_mm_mod_umba.git        %~dp0\umba
+
 
 
